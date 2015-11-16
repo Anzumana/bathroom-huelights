17ccf642be851210ef0d6aa632fc7ad95c57dedc
Anzumana <taal@projektionisten.de>
2015-11-16 11:18:29 +0100

    update for projektionisten

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,8 +4,9 @@ $( document ).ready(function() {
 	setInterval(checkBathroom, 1000);
 
 	function checkBathroom() {
-		$.getJSON( "http://192.168.1.2/api/196d0c80be53def2da8e0d0327395df/lights", function( data ) {
-			var already_taken = data[2].state.reachable;
+		$.getJSON( "http://192.168.0.96/api/196d0c80be53def2da8e0d0327395df/lights", function( data ) {
+			console.log(data);
+			var already_taken = data[3].state.reachable;
 			if(already_taken){
 				$("body").css('background-color', 'red');
 			} else {
--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 Bride takes around 3 seconds to recongnize that bulp is available
 Takes around 30 seconds to realized that it is no longer available
+[yo website generator](https://github.com/yeoman/generator-gulp-webapp#readme)
