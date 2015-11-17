d457dc78a4cd41ead2e75bfb248877c6712036ca
Anzumana <taal@projektionisten.de>
2015-11-17 13:35:32 +0100

    removed post webhook from main branch

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,4 @@
 $( document ).ready(function() {
-	var taken_before = false;
 	setInterval(checkBathroom, 1000);
 
 	function checkBathroom() {
@@ -8,16 +7,10 @@ $( document ).ready(function() {
 			if(already_taken){
 	 			$("body").css('background-color', 'red');
 				$("#favicon").attr("href","favicon_red.ico");
-				taken_before = true;
 			} else {
 				$("body").css('background-color', 'green');
 				$("#favicon").attr("href","favicon_green.ico");
-				if(taken_before){
-					$.post("https://hooks.slack.com/services/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",'payload={"text": "Freee", "channel": "#p_bathroom", "username":"bathroom_bot", "icon_emoji": ":troll:"}');
-				}
-				taken_before = false;
 			}
-			
 		});
 	}
 
